16f58e2560de7f166e231bd58217779a5d548970
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-05 13:14:03 +0100

    Add addAnimal method

--- src/main/java/org/example/repository/AnimalRepository.java
+++ src/main/java/org/example/repository/AnimalRepository.java
@@ -12,4 +12,19 @@ public class AnimalRepository {
         this.connection = connection;
     }
 
+    public int addAnimal(String name, String species) {
+        String query = "INSERT INTO animals VALUES(?, ?)";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, name);
+            ps.setString(2, species);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(String.format("%s was not added to database", name));
+        }
+
+        return 0;
+    }
+
 }
