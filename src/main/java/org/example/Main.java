95ada90179aac98cf9cba57b634c4c43c0a1dda9
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-05 15:29:58 +0100

    Clean up main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,21 @@
 package org.example;
 
+import org.example.repository.AnimalRepository;
+import org.example.userInterface.UserInterface;
 import org.example.utilities.Utilities;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 import java.util.Properties;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         Properties properties = Utilities.getProperties();
+        Scanner scanner = new Scanner(System.in);
 
         String url = properties.getProperty("db.url");
         String username = properties.getProperty("db.username");
@@ -20,9 +26,10 @@ public class Main {
                 username,
                 password
         )) {
-            var meta = conn.getMetaData();
-            System.out.println(meta.getURL());
-            System.out.println(meta.getUserName());
+            AnimalRepository animalRepository = new AnimalRepository(conn);
+            UserInterface ui = new UserInterface(scanner, animalRepository);
+            ui.run();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
