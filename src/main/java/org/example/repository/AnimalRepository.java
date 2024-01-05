fa09846777e0edab237ca75b7a8bb60dcaa149ce
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-05 13:32:41 +0100

    Add displayAnimas method

--- src/main/java/org/example/repository/AnimalRepository.java
+++ src/main/java/org/example/repository/AnimalRepository.java
@@ -2,6 +2,7 @@ package org.example.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AnimalRepository {
@@ -13,18 +14,37 @@ public class AnimalRepository {
     }
 
     public int addAnimal(String name, String species) {
-        String query = "INSERT INTO animals VALUES(?, ?)";
+        String query = "INSERT INTO animals (name, species) VALUES(?, ?)";
 
         try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, name);
             ps.setString(2, species);
             return ps.executeUpdate();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             System.out.println(String.format("%s was not added to database", name));
         }
 
         return 0;
+    }
+
+    public void displayAnimals() {
+        String query = "SELECT * FROM animals";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String species = rs.getString("species");
+                System.out.printf("%-3d | %-10s | %-10s\n", id, name, species);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to display animals");
+        }
     }
 
 }
